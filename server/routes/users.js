b25c979916f5768e11c39c27a2b7c3c022f32aed
Billy Canham <billycanhampt@gmail.com>
2020-10-12 17:32:14 +0200

    authorisation and middleware

---
+++ server/routes/users.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+const { check, validationResult } = require("express-validator");
+
+// route: post api/users
+// description: Register a user
+// access: Public
+
+router.post(
+  "/",
+  [
+    check("username", "Please enter a username").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 8 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // destructure
+    const { username, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      user = new User({
+        username,
+        email,
+        password,
+      });
+      // returns promise
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("server error");
+    }
+  }
+);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const connectDB = require("./config/database");
 
+const app = express();
+// bring in validation
+const { check, validationResult } = require("express-validator");
+
 connectDB();
+// init mddleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.json({ msg: "Welcome slutbag" }));
+
+// // ROUTES
+// // users
+app.use("/api/users", require("./routes/users"));
+// authorisation
+app.use("/api/authorisation", require("./routes/authorisation"));
+// exercises + exercise logs
 
 // set up basic server
-const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log("Server Good to go"));
